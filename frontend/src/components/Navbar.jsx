b6ed9c721b8a309d558de6957fd0ba8a9ddbad19
SucioLoco <irelandrj@yahoo.com>
2022-09-28 15:34:39 -0400

    Fine tuning navbar component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,23 +1,77 @@
 // Imports
 import React from 'react';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 
 // Material.UI Imports
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid'
-import ButtonGroup from '@mui/material/ButtonGroup';
+import Paper from '@mui/material/Paper'
+import Appbar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import AppBar from '@mui/material/AppBar';
+import CarRentalIcon from '@mui/icons-material/CarRental';
 
 
 export default function Navbar () {
+    const barHeight = window.innerHeight / 10
+
        return (
         <Box
         sx={{
-            width: 'auto',
-            height: (window.innerHeight / 10),
-            backgroundColor: '#255dcc',
+            flexgrow: 1,
+            boxShadow: 1,
         }}>
-            <Grid container 
+            <AppBar 
+            position='static'
+            elevation={24}
+            square={false}
+            sx={{
+            width: 'auto',
+            height: barHeight,
+            backgroundColor: '#255dcc',
+            borderBottomLeftRadius: 5,
+            borderBottomRightRadius: 5,
+            }}>
+                <Toolbar>
+                    <CarRentalIcon 
+                    sx={{
+                        fontSize: 50,
+                    }}/>
+                    <Typography
+                    className='logo-title'
+                    variant='h1'
+                    component='div'
+                    sx={{
+                        flexGrow: 1,
+                        fontSize: 50,
+                    }}>
+                        <Link to='/'
+                        style={{
+                            textDecoration: 'none',
+                            color: 'white',
+                            backgroundColor: null,
+                        }}>
+                            Bronco Rental System
+                        </Link>
+                    </Typography>
+                    <Button
+                    sx={{
+                        color: 'white',
+                        backgroundColor: null,
+                    }}>
+                        Login
+                    </Button>
+                    <Button
+                    sx={{
+                        color: 'white',
+                        backgroundColor: null,
+                    }}>
+                        Sign Up
+                    </Button>
+                </Toolbar>
+            </AppBar>
+            {/* <Grid container 
             sx={{
                 alignItems: 'center',
                 padding: 1,
@@ -65,7 +119,7 @@ export default function Navbar () {
                         }}>Register</Button>
                     </ButtonGroup>
                 </Grid>
-            </Grid>
+            </Grid> */}
         </Box>
        )
 }
